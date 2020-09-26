47f3bb961ca203f1e106e2811f9bf55f99afceb4
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-26 18:27:26 +0100

    flask successfully displays index.html

--- flask_prs_hw/app/controller.py
+++ flask_prs_hw/app/controller.py
@@ -4,6 +4,6 @@ from app.models.player import *
 from app.models.prs import *
 
 @app.route('/')
-def hello_world():
-    return "Hello World"
+def index_page():
+    return render_template('index.html')
 
