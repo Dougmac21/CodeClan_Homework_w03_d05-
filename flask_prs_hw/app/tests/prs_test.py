bf49f461ab9c49fa7846df236fdf67b018730390
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-27 16:37:50 +0100

    game tests refactored for dynamic result declarations

--- flask_prs_hw/app/tests/prs_test.py
+++ flask_prs_hw/app/tests/prs_test.py
@@ -18,27 +18,27 @@ class PrsTest(unittest.TestCase):
     def test_play_game_to_victory_player_1(self):
         self.player_1 = Player("Sally", "Paper")
         self.player_2 = Player("John", "Rock")
-        self.assertEqual("Paper wraps Rock. Player 1 wins!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Paper wraps Rock. Sally wins!", Prs.play_game(self, self.player_1, self.player_2))
 
     def test_play_game_to_victory_player_2(self):
         self.player_1 = Player("Cain", "Scissors")
         self.player_2 = Player("Abel", "Rock")
-        self.assertEqual("Rock crushes Scissors. Player 2 wins!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Rock crushes Scissors. Abel wins!", Prs.play_game(self, self.player_1, self.player_2))
 
     def test_game_must_have_valid_choice__player_1_invalid(self):
         self.player_1 = Player("Larry", "Hammer")
         self.player_2 = Player("Paula", "Paper")
-        self.assertEqual("Player 1 must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Larry must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
     
     def test_game_must_have_valid_choice__player_2_invalid(self):
         self.player_1 = Player("Larry", "Paper")
         self.player_2 = Player("Paula", "Hammer")
-        self.assertEqual("Player 2 must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Paula must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
     
     def test_game_must_have_valid_choice__player_1_invalid(self):
         self.player_1 = Player("Larry", "Hammer")
         self.player_2 = Player("Paula", "Shotgun")
-        self.assertEqual("Both players must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Both Larry and Paula must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
     
 
 
