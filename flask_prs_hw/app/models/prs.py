86130b855b2f3bbd9cfead26e90a664836a4e032
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-27 20:43:34 +0100

    Completed CPU Players. Expanded game return strings for context.

--- flask_prs_hw/app/models/prs.py
+++ flask_prs_hw/app/models/prs.py
@@ -17,20 +17,20 @@ class Prs():
             player_2.game_choice = player_2.game_choice.capitalize()
 
         permitted_choices = ("Paper", "Rock", "Scissors", "Lizard", "Spock")
-        cpu_player_hannah_choices = ("Lizard", "Rock")
+        cpu_player_hannah_choices = ("Scissors", "Rock")    # Spock always beats Hannah
+        cpu_player_chris_choices = ("Paper", "Lizard")      # Scissors always beats Chris
         cpu_players = ("Hal", "Morag", "Chris", "Hannah", "Zsolt", "Malcolm", "Harrison", "Cpu")
 
-        winning_choice = "Shotgun"
         if player_1.game_choice == "Paper":
-            winning_choice == "Scissors"
+            winning_choice = "Scissors"
         elif player_1.game_choice == "Rock":
-            winning_choice == "Paper"
+            winning_choice = "Paper"
         elif player_1.game_choice == "Scissors":
-            winning_choice == "Spock"
+            winning_choice = "Spock"
         elif player_1.game_choice == "Lizard":
-            winning_choice == "Rock"
+            winning_choice = "Rock"
         elif player_1.game_choice == "Spock":
-            winning_choice == "Lizard"
+            winning_choice = "Lizard"
 
         import random
         if player_2.name == "Cpu":
@@ -41,7 +41,7 @@ class Prs():
         elif player_2.name == "Morag":
             player_2.game_choice = "Rock"
         elif player_2.name == "Chris":
-            player_2.game_choice = "Scissors"
+            player_2.game_choice = random.choice(cpu_player_chris_choices)
         elif player_2.name == "Hannah":
             player_2.game_choice = random.choice(cpu_player_hannah_choices)
         elif player_2.name == "Zsolt":
@@ -59,59 +59,55 @@ class Prs():
             return f"{player_2.name} must pick from the permitted choices!"
         
         if player_1.game_choice == player_2.game_choice:
-            return f"""
-                    Both {player_1.name} and {player_2.name} have chosen {player_1.game_choice}.
-
-                    That means that it's a tie!
-                    """
+            return f"Both {player_1.name} and {player_2.name} have chosen {player_1.game_choice}. That means that it's a tie!"
 
         elif player_1.game_choice == "Paper":
             if player_2.game_choice == "Rock":
                 return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Paper wraps Rock. {player_1.name} wins!"
             elif player_2.game_choice == "Scissors":
-                return f"Scissors cut Paper. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Scissors cut Paper. {player_2.name} wins!"
             elif player_2.game_choice == "Lizard":
-                return f"Lizard eats Paper. {player_2.name} Wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Lizard eats Paper. {player_2.name} Wins!"
             elif player_2.game_choice == "Spock":
-                return f"Spock is disproved by Paper. {player_1.name} Wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Spock is disproved by Paper. {player_1.name} Wins!"
         
         elif player_1.game_choice == "Rock":
             if player_2.game_choice == "Scissors":
-                return f"Rock crushes Scissors. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Rock crushes Scissors. {player_1.name} wins!"
             elif player_2.game_choice == "Paper":
-                return f"Paper wraps Rock. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Paper wraps Rock. {player_2.name} wins!"
             elif player_2.game_choice == "Lizard":
-                return f"Rock crushes Lizard. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Rock crushes Lizard. {player_1.name} wins!"
             elif player_2.game_choice == "Spock":
-                return f"Spock vaporises Rock. {player_2.name} wins!"                
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Spock vaporises Rock. {player_2.name} wins!"                
         
         elif player_1.game_choice == "Scissors":
             if player_2.game_choice == "Paper":
-                return f"Scissors cut Paper. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Scissors cut Paper. {player_1.name} wins!"
             elif player_2.game_choice == "Rock":
-                return f"Rock crushes Scissors. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Rock crushes Scissors. {player_2.name} wins!"
             elif player_2.game_choice == "Lizard":
-                return f"Lizard is decapitated by Scissors. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Lizard is decapitated by Scissors. {player_1.name} wins!"
             elif player_2.game_choice == "Spock":
-                return f"Spock smashes Scissors. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Spock smashes Scissors. {player_2.name} wins!"
 
         elif player_1.game_choice == "Lizard":
             if player_2.game_choice == "Paper":
-                return f"Lizard eats Paper. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Lizard eats Paper. {player_1.name} wins!"
             elif player_2.game_choice == "Rock":
-                return f"Rock crushes Lizard. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Rock crushes Lizard. {player_2.name} wins!"
             elif player_2.game_choice == "Scissors":
-                return f"Scissors decapitate Lizard. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Scissors decapitate Lizard. {player_2.name} wins!"
             elif player_2.game_choice == "Spock":
-                return f"Lizard poisons Spock. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Lizard poisons Spock. {player_1.name} wins!"
 
         elif player_1.game_choice == "Spock":
             if player_2.game_choice == "Paper":
-                return f"Paper disproves Spock. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Paper disproves Spock. {player_2.name} wins!"
             elif player_2.game_choice == "Rock":
-                return f"Spock vaporises Rock. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Spock vaporises Rock. {player_1.name} wins!"
             elif player_2.game_choice == "Scissors":
-                return f"Spock smashes Scissors. {player_1.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Spock smashes Scissors. {player_1.name} wins!"
             elif player_2.game_choice == "Lizard":
-                return f"Lizard poisons Spock. {player_2.name} wins!"
+                return f"{player_1.name} chose {player_1.game_choice}. {player_2.name} chose {player_2.game_choice}. Lizard poisons Spock. {player_2.name} wins!"
 
