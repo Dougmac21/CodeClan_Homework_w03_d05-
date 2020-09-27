e528fc2f6e1bb4cbfc4b25516083b486129ffc33
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-27 14:10:37 +0100

    flask controller starting to work. had to remove __init__ from Prs class. Game still only returns tie or win. Now need to write conditionals back in.

--- flask_prs_hw/app/controller.py
+++ flask_prs_hw/app/controller.py
@@ -3,11 +3,15 @@ from flask import render_template
 from app.models.player import *
 from app.models.prs import *
 
-@app.route('/')
-def index_page():
-    return render_template('index.html')
+# @app.route('/')
+# def index_page():
+#     return render_template('index.html')
 
 
-@app.route('/<player_1>/<player_1_game_choice>/<player_2>/<player_2_game_choice>')
-def game_page():
-    return render_template('game_page.html')
+@app.route('/<player_1_name>/<player_1_game_choice>/<player_2_name>/<player_2_game_choice>')
+def index_page(player_1_name, player_1_game_choice, player_2_name, player_2_game_choice):
+    player_1 = Player(player_1_name, player_1_game_choice)
+    player_2 = Player(player_2_name, player_2_game_choice)
+    the_game = Prs()
+    game_result = the_game.play_game(player_1, player_2)
+    return render_template('index.html', result_of_game=game_result)
--- flask_prs_hw/app/models/prs.py
+++ flask_prs_hw/app/models/prs.py
@@ -1,12 +1,12 @@
 class Prs():
 
-    def __init__(self, player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
+    # def __init__(self, player_1, player_2):
+    #     self.player_1 = player_1
+    #     self.player_2 = player_2
         
 
-    def play_game(self, player_1_choice, player_2_choice):
-        if player_1_choice == player_2_choice:
+    def play_game(self, player_1, player_2):
+        if player_1.game_choice == player_2.game_choice:
             return "It's a tie!"
         else:
             return "Somebody has won!"
