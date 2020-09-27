1e70c9eefa7202eee5fe82482f0725499dd51e26
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-27 14:23:41 +0100

    refactored prs_test.py tests to match new play_game method

--- flask_prs_hw/app/tests/prs_test.py
+++ flask_prs_hw/app/tests/prs_test.py
@@ -7,18 +7,18 @@ from app.models.prs import Prs
 class PrsTest(unittest.TestCase):
 
     def setUp(self):
-        self.prs = Prs.play_game(self, "Rock", "Paper")
+        pass
 
 
     def test_play_game_to_tie(self):
         self.player_1 = Player("Douglas", "Rock")
         self.player_2 = Player("Marko", "Rock")
-        self.assertEqual("It's a tie!", Prs.play_game(self, self.player_1.game_choice, self.player_2.game_choice))
+        self.assertEqual("It's a tie!", Prs.play_game(self, self.player_1, self.player_2))
 
     def test_play_game_to_victory(self):
         self.player_1 = Player("Sally", "Paper")
         self.player_2 = Player("John", "Rock")
-        self.assertEqual("Somebody has won!", Prs.play_game(self, self.player_1.game_choice, self.player_2.game_choice))
+        self.assertEqual("Player 1 wins!", Prs.play_game(self, self.player_1, self.player_2))
 
 
 
