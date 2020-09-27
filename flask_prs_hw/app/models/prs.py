6494072e284e607830f6c264a6979f746efa7070
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-27 14:20:36 +0100

    refactored play_game method to incorporate all combinations

--- flask_prs_hw/app/models/prs.py
+++ flask_prs_hw/app/models/prs.py
@@ -8,22 +8,21 @@ class Prs():
     def play_game(self, player_1, player_2):
         if player_1.game_choice == player_2.game_choice:
             return "It's a tie!"
-        else:
-            return "Somebody has won!"
 
-
-
-
-
-        # elif player_1_choice == "Rock" and player_2_choice == "Paper":
-        #     return f"{player_2_choice} wraps {player_1_choice}. {self.player_2.name} wins"
-        # elif player_1_choice == "Rock" and player_2_choice == "Scissors":
-        #     return f"{player_1_choice} smashes {player_2_choice}. {self.player_1.name} wins"
-        # elif player_1_choice == "Scissors" and player_2_choice == "Rock":
-        #     return f"{player_2_choice} smashes {player_1_choice}. {self.player_2.name} wins"
-        # elif player_1_choice == "Scissors" and player_2_choice == "Paper":
-        #     return f"{player_1_choice} cuts {player_2_choice}. {self.player_1.name} wins"
-        # elif player_1_choice == "Paper" and player_2_choice == "Scissors":
-        #     return f"{player_2_choice} cuts {player_1_choice}. {self.player_2.name} wins"
-        # elif player_1_choice == "Paper" and player_2_choice == "Rock":
-        #     return f"{player_1_choice} wraps {player_2_choice}. {self.player_1.name} wins"
+        elif player_1.game_choice == "Paper":
+            if player_2.game_choice == "Rock":
+                return "Player 1 wins!"
+            elif player_2.game_choice == "Scissors":
+                return "Player 2 wins!"
+        
+        elif player_1.game_choice == "Rock":
+            if player_2.game_choice == "Scissors":
+                return "Player 1 wins!"
+            elif player_2.game_choice == "Paper":
+                return "Player 2 wins!"
+        
+        elif player_1.game_choice == "Scissors":
+            if player_2.game_choice == "Paper":
+                return "Player 1 wins!"
+            elif player_2.game_choice == "Rock":
+                return "Player 2 wins!"
