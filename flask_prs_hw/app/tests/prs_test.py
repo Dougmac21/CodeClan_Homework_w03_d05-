fe381a31e0d940a0c003e4baf694e08da917da4c
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-28 08:05:19 +0100

    Refactored prs_test.py to match changed reference strings in app.

--- flask_prs_hw/app/tests/prs_test.py
+++ flask_prs_hw/app/tests/prs_test.py
@@ -13,17 +13,17 @@ class PrsTest(unittest.TestCase):
     def test_play_game_to_tie(self):
         self.player_1 = Player("Douglas", "Rock")
         self.player_2 = Player("Marko", "Rock")
-        self.assertEqual("Both Douglas and Marko have chosen the same thing so it's a tie!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Both Douglas and Marko have chosen Rock. That means that it's a tie!", Prs.play_game(self, self.player_1, self.player_2))
 
     def test_play_game_to_victory_player_1(self):
         self.player_1 = Player("Sally", "Paper")
         self.player_2 = Player("John", "Rock")
-        self.assertEqual("Paper wraps Rock. Sally wins!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Sally chose Paper. John chose Rock. Paper wraps Rock. Sally wins!", Prs.play_game(self, self.player_1, self.player_2))
 
     def test_play_game_to_victory_player_2(self):
         self.player_1 = Player("Cain", "Scissors")
         self.player_2 = Player("Abel", "Rock")
-        self.assertEqual("Rock crushes Scissors. Abel wins!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Cain chose Scissors. Abel chose Rock. Rock crushes Scissors. Abel wins!", Prs.play_game(self, self.player_1, self.player_2))
 
     def test_game_must_have_valid_choice__player_1_invalid(self):
         self.player_1 = Player("Larry", "Hammer")
@@ -35,9 +35,9 @@ class PrsTest(unittest.TestCase):
         self.player_2 = Player("Paula", "Hammer")
         self.assertEqual("Paula must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
     
-    def test_game_must_have_valid_choice__player_1_invalid(self):
+    def test_game_must_have_valid_choice__both_players_invalid(self):
         self.player_1 = Player("Larry", "Hammer")
-        self.player_2 = Player("Paula", "Shotgun")
+        self.player_2 = Player("Paula", "Knife")
         self.assertEqual("Both Larry and Paula must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
     
 
