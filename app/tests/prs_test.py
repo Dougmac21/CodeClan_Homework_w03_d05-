d2cb095c7fadca9327c04d5cafa4f176c5300677
Douglas MacIntyre <dougmac21@talk21.com>
2021-01-11 19:56:05 +0000

    Removed human player 2 references and refactored code in tests. All 7 tests pass.

--- app/tests/prs_test.py
+++ app/tests/prs_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from app.models.player import Player
-from app.models.prs import Prs
+from app.models.prs_plus import PrsPlus
 
 
 class PrsTest(unittest.TestCase):
@@ -13,33 +13,19 @@ class PrsTest(unittest.TestCase):
     def test_play_game_to_tie(self):
         self.player_1 = Player("Douglas", "Rock")
         self.player_2 = Player("Marko", "Rock")
-        self.assertEqual("Both Douglas and Marko have chosen Rock. That means that it's a tie!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Both Douglas and Marko have chosen Rock. It's a tie!", PrsPlus.play_game(self, self.player_1, self.player_2))
 
     def test_play_game_to_victory_player_1(self):
         self.player_1 = Player("Sally", "Paper")
         self.player_2 = Player("John", "Rock")
-        self.assertEqual("Sally chose Paper. John chose Rock. Paper wraps Rock. Sally wins!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Sally chose Paper. John chose Rock. Paper wraps Rock. Sally wins!", PrsPlus.play_game(self, self.player_1, self.player_2))
 
     def test_play_game_to_victory_player_2(self):
         self.player_1 = Player("Cain", "Scissors")
         self.player_2 = Player("Abel", "Rock")
-        self.assertEqual("Cain chose Scissors. Abel chose Rock. Rock crushes Scissors. Abel wins!", Prs.play_game(self, self.player_1, self.player_2))
+        self.assertEqual("Cain chose Scissors. Abel chose Rock. Rock crushes Scissors. Abel wins!", PrsPlus.play_game(self, self.player_1, self.player_2))
 
     def test_game_must_have_valid_choice__player_1_invalid(self):
         self.player_1 = Player("Larry", "Hammer")
         self.player_2 = Player("Paula", "Paper")
-        self.assertEqual("Larry must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
-    
-    def test_game_must_have_valid_choice__player_2_invalid(self):
-        self.player_1 = Player("Larry", "Paper")
-        self.player_2 = Player("Paula", "Hammer")
-        self.assertEqual("Paula must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
-    
-    def test_game_must_have_valid_choice__both_players_invalid(self):
-        self.player_1 = Player("Larry", "Hammer")
-        self.player_2 = Player("Paula", "Knife")
-        self.assertEqual("Both Larry and Paula must pick from the permitted choices!", Prs.play_game(self, self.player_1, self.player_2))
-    
-
-
-    
+        self.assertEqual("Larry must pick from the permitted choices!", PrsPlus.play_game(self, self.player_1, self.player_2))
--- run_tests.py
+++ run_tests.py
@@ -6,4 +6,3 @@ from app.tests.prs_test import PrsTest
 
 if __name__ == "__main__":
     unittest.main()
-
