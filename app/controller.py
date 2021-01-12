bd44a70445f53ab8fc7547c69276621477d77747
Douglas MacIntyre <dougmac21@talk21.com>
2021-01-12 15:20:49 +0000

    Game now runnign again on flask form integration

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app.models.player import *
 from app.models.prs_plus import *
 
@@ -7,8 +7,20 @@ from app.models.prs_plus import *
 @app.route('/', methods=['GET', 'POST'])
 def index_page():
     if request.method == 'POST':
-        req = request.form 
 
+        req = request.form
+        print(req)
+
+        player_1_name = request.form.get("player_1")
+        player_1_game_choice = request.form.get("player_1_weapon")
+        cpu_player_name = request.form.get("cpu-player")
+
+        player_1 = Player(player_1_name, player_1_game_choice)
+        cpu_player = Player(cpu_player_name, None)
+        the_game = PrsPlus()
+        game_result = the_game.play_game(player_1, cpu_player)
+        
+        return render_template('index.html', result_of_game=game_result)
 
     return render_template('index.html')
 
@@ -23,4 +35,4 @@ def cpu_page(player_1_name, player_1_game_choice, cpu_player_name):
     cpu_player = Player(cpu_player_name, None)
     the_game = PrsPlus()
     game_result = the_game.play_game(player_1, cpu_player)
-    return render_template('play.html', result_of_game=game_result)
+    return render_template('index.html', result_of_game=game_result)
